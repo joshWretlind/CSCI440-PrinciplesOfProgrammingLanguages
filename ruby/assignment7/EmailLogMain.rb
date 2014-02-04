aa7b131c8914571b8e6c14ebc77d2cd940325f5f
Josh Wretlind <jwretlin@mymail.mines.edu>
2014-02-04 10:59:41 -0700

    updating assignment7 with tests

--- ruby/assignment7/EmailLogMain.rb
+++ ruby/assignment7/EmailLogMain.rb
@@ -53,30 +53,40 @@ class EmailData
 	end
 end
 
-lines=IO.readlines("mail.log")
-count = 0
-idTable = {}
+def readFile(fileName)
+	lines=IO.readlines(fileName)
+	count = 0
+	idTable = {}
 
-lines.each do |line|
-	if(line =~ /qmgr.*from/)
-		count += 1
-		id = line.match(/\: .*\: f/)[0].gsub(/[: f]/,"")
-		from = line.match(/<.*>/)[0].gsub(/[<>]/,"")
-		size = line.match(/size=\d+/)[0].gsub(/size=/,"")
-		date = line.match(/.* to/)[0].gsub(/to/,"")
-		idTable[id] = EmailData.new(from,id,date,size)
-	elsif(line =~ /smtp/)
-		id = line.match(/\: .*\: t/)[0].gsub(/[: t]/,"")
-		to = line.match(/<.*>,/)[0].gsub(/[<>, ]/,"")
-		to = to.slice(0,to.length-1)
-		puts "#{id} , #{to} , #{idTable[id]}"
-		idTable[id].addToAddress(to)
+	lines.each do |line|
+		if(line =~ /qmgr.*from/)
+			count += 1
+			id = line.match(/\: .*\: f/)[0].gsub(/[: f]/,"")
+			from = line.match(/<.*>/)[0].gsub(/[<>]/,"")
+			size = line.match(/size=\d+/)[0].gsub(/size=/,"")
+			date = line.match(/.* to/)[0].gsub(/to/,"")
+			idTable[id] = EmailData.new(from,id,date,size)
+		elsif(line =~ /smtp/)
+			id = line.match(/\: .*\: t/)[0].gsub(/[: t]/,"")
+			to = line.match(/<.*>,/)[0].gsub(/[<>, ]/,"")
+			to = to.slice(0,to.length-1)
+			puts "#{id} , #{to} , #{idTable[id]}"
+			idTable[id].addToAddress(to)
 
+		end
 	end
+	return count, idTable
 end
 
-idTable.each do |value, entry|
-	puts entry
-end 
+def printReport(idTable, count)
 
-puts "Total number of  emails: #{count}"
+	idTable.each do |value, entry|
+		puts entry
+	end 
+
+	puts "Total number of  emails: #{count}"
+
+end
+
+
+readFile("mail.log")
--- ruby/assignment7/EmailLogTester.rb
+++ ruby/assignment7/EmailLogTester.rb
@@ -1,1 +1,18 @@
+require 'test/unit'
+require './EmailLogMain'
 
+class EmailLogTester < Test::Unit::TestCase
+	def test_size
+		testCount = 0
+		idTable = {}
+		testCount, testTable = readFile("mail.log")
+		assert_equal(61, testCount)
+		assert_equal(61, testTable.size)
+	end	
+
+	def test_ids
+		testTable.each do |key, value| 
+		puts value
+		assert_equal(key, value.messageID)
+	end
+end
