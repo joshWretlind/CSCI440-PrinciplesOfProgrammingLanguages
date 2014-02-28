99512b73bd728a0715542349e05716aba5f9e43f
Josh Wretlind <jwretlin@mymail.mines.edu>
2014-02-28 16:17:52 -0700

    finishing up assignment 1

--- haskel/assignment1/chapter1.hs
+++ haskel/assignment1/chapter1.hs
@@ -1,3 +1,5 @@
+receipt = [(3, 4), (5, 6)]
+
 negsInList x = sum [1 | i <- x, (i < 0)]
 
 oddsInList x = sum [1 | i <- x, (mod i 2) == 1]
@@ -11,3 +13,11 @@ getQuantities x = [ fst i | i <- x]
 getPrices x = [ snd i | i <- x]
 
 getTotal x = [(fst i) * (snd i) | i <- x ]
+
+doubleAll x = [[j*2 | j <- i] | i <- x]
+
+doubleFirstList x = [ 2*i | i <- (head x)]
+
+doubleSecondList x = [ 2*i | i <- (x !! 1)]
+
+sumListItems x = [sum([j | j <- i]) | i <- x]
