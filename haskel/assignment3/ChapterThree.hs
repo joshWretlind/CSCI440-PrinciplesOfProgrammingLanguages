23cd91064629ecc7caa6389d4a9892fa1db754a1
Josh Wretlind <jwretlin@mymail.mines.edu>
2014-03-04 11:35:37 -0700

    continuing work on assignment 3 -- adding in use of pattern matching in argument list

--- haskel/assignment3/ChapterThree.hs
+++ haskel/assignment3/ChapterThree.hs
@@ -8,3 +8,6 @@ square x = x^2
 
 squareEvenNumbers :: [Int] -> [Int]
 squareEvenNumbers x = [square i | i <- x, (mod i 2) == 0]
+
+courseMajor :: [Char] -> [Char]
+courseMajor all@(x:y:xs) = all ++ " is a " ++ [x] ++ [y] ++ " course"
