c65149ed907d424cdee1c5c4ae0fe66387b2cb7c
Josh Wretlind <jwretlin@mymail.mines.edu>
2014-03-20 12:10:20 -0600

    fixing more code

---
+++ haskel/assignment5a/ChapterFive_pt1.hs
@@ -0,0 +1,7 @@
+module ChapterFive_Pt1 where
+
+convert input factor = input*factor
+
+doMetersToFeet = convert 3.28084
+
+doMilesToKM = convert 1.607
--- haskel/playAndSharePart5a.hs
+++ haskel/playAndSharePart5a.hs
@@ -8,12 +8,12 @@ initials x = filter (`elem` ['A'..'Z']) x
 
 noEven x = map (filter odd) x
 
-doubleArea h w = (multThree 2) h w
+doubleArea = multThree 2
 
 multThreeF :: Float -> Float -> Float -> Float
 multThreeF x y z = x*y*z
 
 tenPctDiscount :: Float -> Float -> Float
-tenPctDiscount x y = ((multThreeF 0.1) x) y
+tenPctDiscount x y = multThreeF 0.1
 
 pctDiscount x = multThreeF x
