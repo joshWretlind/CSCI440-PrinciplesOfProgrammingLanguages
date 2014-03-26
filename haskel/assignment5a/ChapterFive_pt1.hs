4a9676f56f8a5b0db7605888f777c2a6f9881804
Josh Wretlind <jwretlin@mymail.mines.edu>
2014-03-25 19:31:06 -0600

    adding in assignment 5a and some other things

--- haskel/assignment5a/ChapterFive_pt1.hs
+++ haskel/assignment5a/ChapterFive_pt1.hs
@@ -5,3 +5,21 @@ convert input factor = input*factor
 doMetersToFeet = convert 3.28084
 
 doMilesToKM = convert 1.607
+
+calcSalesTax x y = x*y
+
+doGolden = calcSalesTax 0.03
+
+doBoulder = calcSalesTax 0.0341
+
+swap (a,b) = (b,a)
+
+swapAll x = map swap x
+
+applyIfTrue x y z 
+  | z = x y
+  | otherwise = 0
+  
+calcArea x y = x*y
+
+calcAreas x y = zipWith calcArea x y 
